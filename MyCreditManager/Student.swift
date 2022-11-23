1371811096497c9852cf5d7ffb2abde31b13ac1b
joy <suebin710@gmail.com>
2022-11-23 22:09:04 +0900

    add: Student 객체

---
+++ MyCreditManager/Student.swift
@@ -0,0 +1,14 @@
+//
+//  Student.swift
+//  MyCreditManager
+//
+//  Created by exception on 2022/11/23.
+//
+
+import Foundation
+
+struct Student {
+    var name: String
+    var score: [[String: String]]?
+    var grade: Float?
+}
