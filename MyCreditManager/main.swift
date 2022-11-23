93d1a1870d73f67ad8c54e90f272fb06071f8432
joy(sbcheon) <suebin710@gmail.com>
2022-11-23 12:28:10 +0900

    add: startManager
    - 각 기능 메소드 뼈대 구축 및 startManager 선택지 구현

--- MyCreditManager/main.swift
+++ MyCreditManager/main.swift
@@ -7,5 +7,62 @@
 
 import Foundation
 
-print("Hello, World!")
+func startManager() {
+    print(Constants.Message.startMessage)
+    print(Constants.Message.selectMessage)
+    
+    let menuPickInput = readLine()
+    
+    switch menuPickInput {
+    case "1":
+        addStudent()
+    case "2":
+        deleteStudent()
+    case "3":
+        addOrEditScore()
+    case "4":
+        deleteScore()
+    case "5":
+        printGrade()
+    case "X":
+        exitProgram()
+    default:
+        print(Constants.ErrorMessage.selectInputErrorMessage)
+        if menuPickInput != nil {
+            startManager()
+        }
+    }
+}
 
+func addStudent() {
+    print(Constants.Message.addStudentNameInputMessage)
+    
+    startManager()
+}
+
+func deleteStudent() {
+    print(Constants.Message.deleteStudentNameInputMessage)
+    startManager()
+}
+
+func addOrEditScore() {
+    print(Constants.Message.addScoreInputMessage)
+    startManager()
+}
+
+func deleteScore() {
+    print(Constants.Message.deleteScoreMessage)
+    startManager()
+}
+
+func printGrade() {
+    print(Constants.Message.getGradeMessage)
+    startManager()
+}
+
+func exitProgram() {
+    print(Constants.Message.programExitMessage)
+    exit(0)
+}
+
+startManager()
