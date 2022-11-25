8c1757f50cfd5efdbb36b51134d753b180bab5a1
joy <suebin710@gmail.com>
2022-11-25 22:52:56 +0900

    add: printGrade

--- MyCreditManager/main.swift
+++ MyCreditManager/main.swift
@@ -132,7 +132,56 @@ func deleteScore() {
 
 func printGrade() {
     print(Constants.Message.getGradeMessage)
+    
+    let name = readLine() ?? ""
+    
+    if name.trimmingCharacters(in: .whitespaces).count > 0 {
+        let trimmedName = name.replacingOccurrences(of: " ", with: "")
+        print(calculateGrade(name: trimmedName))
+    } else {
+        print(Constants.ErrorMessage.inputErrorMessage)
+    }
+    
     startManager()
+}
+
+func calculateGrade(name: String) -> Double {
+    var countSum = 0.0
+    var subjectCount = 0.0
+    
+    if let idx = studentList.firstIndex(where: { $0.name == name }) {
+        studentList[idx].score.forEach {
+            countSum += scoreToFloat(score: $0.value)
+        }
+        subjectCount = Double(studentList[idx].score.count)
+    } else {
+        print(name + Constants.ErrorMessage.studentNotFoundMessage)
+    }
+    
+    return round((countSum / subjectCount) * 100) / 100
+}
+
+func scoreToFloat(score: String) -> Double {
+    switch score {
+    case "A+":
+        return 4.5
+    case "A":
+        return 4.0
+    case "B+":
+        return 3.5
+    case "B":
+        return 3.0
+    case "C+":
+        return 2.5
+    case"C":
+        return 2.0
+    case "D+":
+        return 1.5
+    case "D":
+        return 1.0
+    default:
+        return 0.0
+    }
 }
 
 /// 임시 메소드 -> 리스트 파악용
